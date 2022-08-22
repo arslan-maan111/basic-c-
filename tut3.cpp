ac528ec3f94dc8d294e05359ff7504f9bd0568a1
arslan-maan111 <111747058+arslan-maan111@users.noreply.github.com>
2022-08-22 16:20:12 +0500

    Add files via upload

---
+++ tut3.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+// This comment is for chekcing how to put comment in the program
+/* this comment is for 
+multiple comment
+check in the 
+program */
+int main(){
+    cout<<"Hello World";
+    return 0;
+}
---
+++ tut4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // int a = 14;
+    // int b = 15;
+    int a=14, b=15;
+    cout<<"The value of a is " <<a  <<".The value of b is " << b;
+    return 0; 
+}
