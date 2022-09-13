ed6868709e162a37d691b4bfd2e303f86089f26c
arslan-maan111 <111747058+arslan-maan111@users.noreply.github.com>
2022-09-13 10:37:04 +0500

    Add files via upload

---
+++ tut15.cpp
@@ -0,0 +1,37 @@
+// #include <iostream>
+// using namespace std;
+
+// // ****** Function prototype
+// // syntax of funtion: type function-name (arguments);
+// // int sum(int a, int b); //--> Acceptable
+// // int sum(int a, b); //--> Not Acceptable 
+// // int sum(int , int );  --> Accetable
+// int sum(int, int);    
+// // void g(void); //--> Acceptable
+// void g(int c);
+
+// int main(){
+//     int num1, num2;
+//     cout<<"Enter the first no: " <<num1 <<endl;
+//     cin>>num1;
+//     cout<<"Enter the second no: " <<num2 <<endl;
+//     cin>>num2;
+
+//         // num1 and num2 are actual parameters
+//     sum(num1, num2);
+//      return 0;
+//     }
+
+//     void sum(int a , int b){
+// // Formal Parameters a and b will be taking values from actual parameters num1 and num2.
+
+//     int c=a+b;
+//     g(c);
+// // cout<<"sum is" << a+b;
+//     }
+     
+//     void g(int c){
+//         cout<<" sum is: "<<c; 
+//     }
+   
+   
